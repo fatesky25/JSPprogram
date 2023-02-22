0e2dd4ae1db96fcbc0cbde3320c1c9a18d28abb2
fatesky25 <fatesky26@gmail.com>
2023-02-22 18:02:45 +0900

    20230222

---
+++ ServletProject/src/main/java/boardtwo/action/ContentAction.java
@@ -0,0 +1,30 @@
+package boardtwo.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import boardtwo.model.BoardDAO;
+import boardtwo.model.BoardVO;
+
+// 글 내용 처리
+public class ContentAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+        
+		// 해당 글 번호
+		int num = Integer.parseInt(request.getParameter("num"));
+		// 해당 페이지 번호
+		String pageNum = request.getParameter("pageNum");
+		 
+	    BoardDAO dbPro = BoardDAO.getInstance();
+	    BoardVO article = dbPro.getArticle(num);
+		
+	    request.setAttribute("num", new Integer(num));
+	    request.setAttribute("pageNum", new Integer(pageNum));
+		request.setAttribute("article", article);
+		
+		return "/boardtwo/content.jsp";
+	}
+
+}
--- ServletProject/src/main/java/mvcmem/control/ActionFactory.java
+++ ServletProject/src/main/java/mvcmem/control/ActionFactory.java
@@ -1,78 +1,101 @@
 package mvcmem.control;
 
+import mvcmem.action.DeleteFormAction;
+import mvcmem.action.DeleteProcAction;
 import mvcmem.action.IdCheckAction;
 import mvcmem.action.IndexAction;
 import mvcmem.action.LoginFormAction;
 import mvcmem.action.LoginProcAction;
 import mvcmem.action.LogoutAction;
 import mvcmem.action.ModifyFormAction;
+import mvcmem.action.ModifyProcAction;
 import mvcmem.action.RegFormAction;
 import mvcmem.action.RegProcAction;
 import mvcmem.action.ZipCheckAction;
 
 public class ActionFactory {
 
-	private static ActionFactory factory;
-
-	private ActionFactory() {
-	}
-
+private static ActionFactory factory;
+	
+	private ActionFactory() { }
+	
 	public static synchronized ActionFactory getInstance() {
-
-		if (factory == null) {
+		
+		if(factory == null) {
 			factory = new ActionFactory();
 		}
 		return factory;
 	}
-
+	
 	public Action getAction(String cmd) {
-
+		
 		Action action = null;
-
-		switch (cmd) {
-
-		case "index":
+		
+		switch(cmd) {
+		
+		case "index" :
 			action = new IndexAction();
 			break;
-
-		case "regForm":
+			
+		case "regForm" :
 			action = new RegFormAction();
 			break;
-
-		case "idCheck":
+			
+		case "idCheck" :
 			action = new IdCheckAction();
 			break;
-
-		case "regProc":
+			
+		case "regProc" :
 			action = new RegProcAction();
-			break;
-
-		case "zipCheck":
+			break;	
+			
+		case "zipCheck" :
 			action = new ZipCheckAction();
-			break;
-
-		case "login":
+			break;		
+			
+		case "login" :
 			action = new LoginFormAction();
-			break;
-
-		case "loginProc":
+			break;		
+		
+		case "loginProc" :
 			action = new LoginProcAction();
-			break;
-
-		case "logout":
+			break;	
+			
+			
+		case "logout" :
 			action = new LogoutAction();
-			break;
-
-		case "modifyForm":
+			break;		
+			
+			
+		case "modifyForm" :
 			action = new ModifyFormAction();
-			break;
-
-		default:
+			break;		
+			
+		case "modifyProc" :
+			action = new ModifyProcAction();
+			break;		
+			
+			
+		case "deleteForm" :
+			action = new DeleteFormAction();
+			break;		
+			
+		case "deleteProc" :
+			action = new DeleteProcAction();
+			break;		
+			
+			
+		default :
 			action = new IndexAction();
 			break;
 		}
-
+	
 		return action;
 	}
-
+	
+	
+	
+	
+	
+	
 }
